782f171461a5a3e36d799b9d90da06cb7f438233
Chidinma Odoh <chidinmaodoh29@gmail.com>
2024-04-10 19:35:29 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-
+<footer>This project was coded by {" "}
+             <a href="https://github.com/Dhymarh" target="_blank" rel= "noreferrer" > Chidinma Odoh</a>, is {" "}
+             <a href="https://github.com/Dhymarh/react-weather-app" target="_blank" rel= "noreferrer">open-sourced on Github</a> and <a href="https://my-react-weather-app-pi.vercel.app/" target="_blank" rel= "noreferrer">hosted on Vercel.</a>
+        </footer>
+   
     </div>
   );
 }
